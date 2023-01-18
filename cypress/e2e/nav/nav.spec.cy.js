03ae948743d3da9cd2f2b365da80d17b5b30dad1
chidinma agbazuaka <catherine.chidinma@dcthomson.co.uk>
2023-01-18 12:08:31 +0000

    'changes'

--- cypress/e2e/nav/nav.spec.cy.js
+++
@@ -1,7 +0,0 @@
-//import { loginPage } from "./login";
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-
-Given('User is on EV page', () => {
-    //loginPage.visitPage()
-    cy.visit('/')
-  })
